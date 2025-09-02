098307658d64fe68281bb90b884e4dbe55298bd2
tarncairn <193822639+tarncairn@users.noreply.github.com>
2025-09-02 08:36:56 -0600

    use groups for player

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
-        player.update(dt)
+
+
+        for item in drawable:
+            item.draw(screen)
+
+
+        updatable.update(dt)
+
+
         pygame.display.flip()
+
+
         dt = clock.tick(60) / 1000
     
 
--- player.py
+++ player.py
@@ -3,6 +3,7 @@ import pygame
 from circleshape import CircleShape
 
 class Player(CircleShape):
+    
     def __init__(self, x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
